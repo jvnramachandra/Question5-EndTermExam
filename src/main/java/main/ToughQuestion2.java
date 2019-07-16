d593dffa77d0a646f2f7d654ade85d1d3ffe082b
jvnra <jvnra@Laptop-DellG5>
2019-07-16 09:57:42 +0530

    Done

--- src/main/java/main/ToughQuestion2.java
+++ src/main/java/main/ToughQuestion2.java
@@ -1,15 +1,16 @@
 package main;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
  
-class ToughQuestion2 {
+class ToughQuestions2 {
             public static void main(String[] args) throws NumberFormatException, IOException {
                         Integer bread;
                         Integer butter;
                         Integer jam;
+                        int tcal;
+                        float tenergy;
                         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                         Calories cal=new Calories();
                         System.out.println("1.Bread only\n2.Bread+Jam\n3.Bread+Jam+Butter");
@@ -20,14 +21,20 @@ class ToughQuestion2 {
                         case 1 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread
-                        	System.out.println();
+                        	tcal = cal.calculateCalories(bread);
+                        	tenergy = cal.calculateEnergy(tcal);
+                        	System.out.printf("%.3f",tenergy);
+                            System.out.println(" kJ of energy generated from "+tcal+" calories");
                         	break;
                         case 2 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	System.out.println("Enter the number teaspoon of Jam");
                         	jam = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread & jam
-                        	System.out.println();
+                        	tcal = cal.calculateCalories(bread,jam);
+                        	tenergy = cal.calculateEnergy(tcal);
+                        	System.out.printf("%.3f",tenergy);
+                            System.out.println(" kJ of energy generated from "+tcal+" calories");
                         	break;
                         case 3 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
@@ -36,7 +43,10 @@ class ToughQuestion2 {
                         	System.out.println("Enter the number teaspoon of Butter");
                         	butter = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for  bread,jam & butter
-                        	System.out.println();
+                        	tcal = cal.calculateCalories(bread,jam,butter);
+                        	tenergy = cal.calculateEnergy(tcal);
+                        	System.out.printf("%.3f",tenergy);
+                            System.out.println(" kJ of energy generated from "+tcal+" calories");
                         	break;
                         }
                         //System.out.printf("%.3f",ca.calculateEnergy());
@@ -45,9 +55,33 @@ class ToughQuestion2 {
 }
 class Calories {
             
-            int calculateCalories() {
+            int calculateCalories(int bpiece) {
             //you have to overload this function for three parameters refer the question
-            	return -1;
+            	int breadcal = 74;
+            	int tcal = breadcal * bpiece;
+            	//calculateEnergy(tcal);
+            	return tcal;
+            }
+            int calculateCalories(int bpiece,int jpiece) {
+                //you have to overload this function for three parameters refer the question
+            	int breadcal = 74, jamcal = 26;	
+            	int tcal = (bpiece * breadcal) + (jpiece * jamcal);
+            	//calculateEnergy(tcal);
+            	
+                return tcal;
+            }
+            int calculateCalories(int bpiece,int jpiece,int btrpiece) {
+                //you have to overload this function for three parameters refer the question
+            	int breadcal = 74, butcal = 102, jamcal = 26;
+            	int tcal = (bpiece * breadcal) + (jpiece * jamcal) + (btrpiece * butcal);
+            	//calculateEnergy(tcal);
+                return tcal;
+            }
+            
+            float calculateEnergy(int tcal){
+            	float energy = (float) (tcal * 4.18680);
+				return energy;
+            	
             }
             
             
